585dd4261346608f2ce76279460278d491c48f84
narendhar <gujjulanarendharreddy@gmail.com>
2026-04-07 22:47:13 +0530

    changed app.py return statement

--- Simple-Python-App-Deploy/app.py
+++ Simple-Python-App-Deploy/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world!'
+    return 'Hello, This is a sample flask service to practice  ci/cd'
 
 if __name__ == '__main__':
     app.run()
